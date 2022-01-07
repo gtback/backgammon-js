f3ac6cb9cd79f2bcea677b5cdab803b431c29a10
Greg Back <git@gregback.net>
2022-01-06 21:40:20 -0500

    Make colors configurable

--- board.js
+++ board.js
@@ -3,25 +3,36 @@ brown = 'rgb(153,102,51)';
 red = 'rgb(255, 0, 0)';
 white = 'rgb(255, 255, 255)';
 
-player1 = {
-    checkerColor: 'rgb(0, 127, 255)'
-};
+blue = 'rgb(0, 127, 255)';
+yellow = 'rgb(255, 255, 0)';
 
-player2 = {
-    checkerColor: 'rgb(255, 255, 0)'
+const DEFAULT_OPTIONS = {
+    frameColor: brown,
+    boardBackground: black,
+    // We start counting at 0, so the "oddPoints" are at index 0, 2, 4, ... but
+    // are points 1, 3, 5, ...
+    oddPoints: red,
+    evenPoints: white,
+    player1: {
+        checkerColor: blue
+    },
+    player2: {
+        checkerColor: yellow,
+    },
 };
 
 class Point {
-    constructor(index, board) {
-        this.board = board;
+    constructor(index, board, opts) {
         this.index = index;
+        this.board = board;
+        this.opts = opts;
 
         if (this.index >= 12) { // Points on the far (Player 2) side of the board
             this.startX = this.board.x;
             this.xDirection = 1; // left to right
             this.yDirection = 1; // downward
             this.baseLine = this.board.y;
-            this.textPoint = this.board.y - 5;
+            this.textPoint = this.board.y - 6;
         }
         else { // Points on the near (Player 1) side of the board
             this.startX = this.board.x + this.board.width;
@@ -41,8 +52,9 @@ class Point {
     draw(ctx) {
         const pointGap = 6;
 
-        ctx.strokeStyle = this.index % 2 == 0 ? white : red;
-        ctx.fillStyle = this.index % 2 == 0 ? red : white;
+        ctx.fillStyle = this.index % 2 == 0 ? this.opts.oddPoints : this.opts.evenPoints;
+        // The stroke style is the opposite of the fill style
+        ctx.strokeStyle = this.index % 2 == 0 ? this.opts.evenPoints : this.opts.oddPoints;
 
         const pointHeight = this.board.height * 0.40;
         let tip = this.baseLine + this.yDirection * pointHeight;
@@ -56,7 +68,7 @@ class Point {
         ctx.lineTo(this.xInit + this.xDirection * pointGap / 2, this.baseLine);
         ctx.fill();
 
-        // Label point number
+        // Label point number (always black, not configurable)
         ctx.fillStyle = black;
         ctx.fillText(this.index + 1, this.midpoint, this.textPoint);
     }
@@ -66,6 +78,8 @@ class Diagram {
     constructor(canvas) {
         this.canvas = canvas;
         this.ctx = canvas.getContext('2d');
+
+        this.opts = DEFAULT_OPTIONS;
 
         const canvasWidth = this.canvas.width = 640;
         const canvasHeight = this.canvas.height = 480;
@@ -106,7 +120,7 @@ class Diagram {
 
         this.points = Array();
         for (let point = 0; point < 24; point++) {
-            this.points.push(new Point(point, this.board));
+            this.points.push(new Point(point, this.board, this.opts));
         }
 
         this.radius = this.board.width / 32;
@@ -118,30 +132,30 @@ class Diagram {
         this.drawBoard();
 
         // Draw Opening Position
-        this.drawCheckers(6, 5, player1);
-        this.drawCheckers(8, 3, player1);
-        this.drawCheckers(13, 5, player1);
-        this.drawCheckers(24, 2, player1);
+        this.drawCheckers(6, 5, this.opts.player1);
+        this.drawCheckers(8, 3, this.opts.player1);
+        this.drawCheckers(13, 5, this.opts.player1);
+        this.drawCheckers(24, 2, this.opts.player1);
 
-        this.drawCheckers(25 - 6, 5, player2);
-        this.drawCheckers(25 - 8, 3, player2);
-        this.drawCheckers(25 - 13, 5, player2);
-        this.drawCheckers(25 - 24, 2, player2);
+        this.drawCheckers(25 - 6, 5, this.opts.player2);
+        this.drawCheckers(25 - 8, 3, this.opts.player2);
+        this.drawCheckers(25 - 13, 5, this.opts.player2);
+        this.drawCheckers(25 - 24, 2, this.opts.player2);
     }
 
     drawCanvas() {
         this.ctx.lineWidth = 5;
-        this.ctx.strokeStyle = black;
+        this.ctx.strokeStyle = black; // always a black border around the canvas
         this.ctx.strokeRect(0, 0, this.canvas.width, this.canvas.height);
     }
 
     drawFrame() {
-        this.ctx.fillStyle = brown;
+        this.ctx.fillStyle = this.opts.frameColor;
         this.ctx.fillRect(this.frame.x, this.frame.y, this.frame.width, this.frame.height);
     }
 
     drawBoard() {
-        this.ctx.fillStyle = black;
+        this.ctx.fillStyle = this.opts.boardBackground;
         this.ctx.fillRect(this.board.x, this.board.y, this.board.width + this.board.barThickness, this.board.height);
 
         this.ctx.lineWidth = 2;
@@ -156,7 +170,7 @@ class Diagram {
         this.ctx.strokeRect(this.board.x, this.board.y, this.board.width + this.board.barThickness, this.board.height);
 
         // Draw Bar
-        this.ctx.fillStyle = brown;
+        this.ctx.fillStyle = this.opts.frameColor;
         this.ctx.fillRect(this.board.x + this.board.width / 2, this.frame.y, this.board.barThickness, this.frame.height);
     }
 
